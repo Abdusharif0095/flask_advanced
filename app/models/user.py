e07b1e46166ff282491e8c5500f57cf64b85c48d
Abdusharif0095 <abdurafiki.abdusharif@gmail.com>
2024-06-23 04:31:39 +0300

    added migrations

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,12 @@
+from datetime import datetime
 from ..extensions import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(50), default='student')
     name = db.Column(db.String(50))
+    login = db.Column(db.String(50))
+    password = db.Column(db.String(200))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    avatar = db.Column(db.String(200))
--- app/routes/post.py
+++ app/routes/post.py
@@ -1,14 +1,65 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
+
 from ..extensions import db
 from ..models.post import Post
 
 post = Blueprint('post', __name__)
 
 
-@post.route('/post/<subject>')
-def create_subject(subject):
-    post = Post(subject=subject)
-    db.session.add(post)
-    db.session.commit()
+@post.route('/')
+def all():
+    posts = Post.query.order_by(Post.date.desc()).all()
+    return render_template('post/all.html', posts=posts)
 
-    return 'Subject Created Successfully!!!'
+
+@post.route('/post/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        teacher = request.form.get('teacher')
+        subject = request.form.get('subject')
+        student = request.form.get('student')
+
+        post = Post(teacher=teacher, subject=subject, student=student)
+
+        try:
+            db.session.add(post)
+            db.session.commit()
+
+            return redirect('/')
+        except Exception as e:
+            print(str(e))
+
+        return redirect('/')
+    else:
+        return render_template('post/create.html')
+
+
+@post.route('/post/<int:id>/update', methods=['POST', 'GET'])
+def update(id):
+    post = Post.query.get(id)
+
+    if request.method == 'POST':
+        post.teacher = request.form.get('teacher')
+        post.subject = request.form.get('subject')
+        post.student = request.form.get('student')
+
+        try:
+            db.session.commit()
+            redirect('/')
+        except Exception as e:
+            print(str(e))
+    else:
+        return render_template('post/update.html', post=post)
+
+
+@post.route('/post/<int:id>/delete', methods=['POST', 'GET'])
+def delete(id):
+    post = Post.query.get(id)
+
+    try:
+        db.session.delete(post)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        print(str(e))
+        return str(e)
