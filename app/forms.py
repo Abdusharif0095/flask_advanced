d1da1f7d47f273d870c920045b133e84874a4636
Abdusharif0095 <abdurafiki.abdusharif@gmail.com>
2024-06-23 05:36:06 +0300

    added registration route

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField('ФИО', validators=[DataRequired(), Length(min=2, max=100)])
+    login = StringField('Логин', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
+    avatar = FileField('Загрузите свое фото', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    submit = SubmitField('Зарегистрироваться')
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect
 from ..extensions import db
 from ..models.user import User
+from ..forms import RegistrationForm
 
 user = Blueprint('user', __name__)
 
@@ -12,3 +13,16 @@ def create_user(name):
     db.session.commit()
 
     return 'User Created Successfully!!!'
+
+
+@user.route('/user/register')
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print(form.name.data)
+        print(form.password.data)
+        print(form.avatar.data)
+        return redirect('/')
+    else:
+        print('Registration error!!!')
+    return render_template('user/register.html', form=form)
